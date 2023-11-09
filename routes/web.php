82272972a7898eb1395aef0bb5dc1e13be19362f
Jorge Ibáñez <jorge.ibanez.1997@gmail.com>
2023-11-09 20:34:15 +0100

    breez

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/categorias', function () {
+    return '<strong>welcome</strong>';
+});
