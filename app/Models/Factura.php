7f487792d0fa12459bfe71469630e1b4ea80d152
Jorge Ibáñez <jorge.ibanez.1997@gmail.com>
2024-01-10 17:50:38 +0100

    10ene

---
+++ app/Models/Factura.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Factura extends Model
+{
+    use HasFactory;
+
+
+    public function  user(){
+
+    }
+}
